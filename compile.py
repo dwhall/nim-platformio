f21f11d480286f71d6195df649ecf2f07d867a2f
!!Dean <dwhall256@gmail.com>
2024-09-05 23:58:58 -0500

    Move build flags to project-side nim.cfg and support more micros
    Previously, the nim compiler build flags were in the Python script.
    Since command-line settings prevent the use of any config file settings,
    this tool would override a user's build configuration.  We should avoid this.
    
    This commit moves the build flags to a new nim.cfg file that
    is copied to the user's project folder if one does not exist.
    This allows the user to change the build settings as needed.
    Three build flags, path, cpu and nimcache, must be programmatically
    appended into the config file because parts of the value come from
    the platformio environment values in the Python script.
    
    I added the "opt:size" build flag to nim.cfg.
    
    I also added support for more microcontroller families.
    A lookup table is used to convert the $PIOPLATFORM
    variable to the CPU type used by the Nim compiler.
    The result is put in the "cpu" build flag in the config file.

--- compile.py
+++ compile.py
@@ -3,13 +3,13 @@ from shutil import copyfile
 from pathlib import Path
 
 Import("env")
+prj_dir = Path(env.subst("$PROJECT_DIR"))
+prj_src_dir = Path(env.subst("$PROJECT_SRC_DIR"))
 
 def copy_files():
     """Copies a minimum set of files needed to compile
     if they do not already exist in the project.
     """
-    prj_dir = Path(env.subst("$PROJECT_DIR"))
-    prj_src_dir = Path(env.subst("$PROJECT_SRC_DIR"))
     files_to_copy = (
         ("nim.cfg", prj_dir),
         ("panicoverride.nim", prj_src_dir),
@@ -25,20 +25,25 @@ def append_options_to_config_file():
     """Appends the PlatformIO platform support files path and cpu options
     to the config file if they are not already present
     """
-    lib_deps = f'"{env.subst("$PROJECT_LIBDEPS_DIR/$PIOENV")}"'
-    fwd_slash_lib_deps = lib_deps.replace("\\", "/")
+    lib_deps_in_quotes = f'"{env.subst("$PROJECT_LIBDEPS_DIR/$PIOENV")}"'
+    fwd_slash_lib_deps_in_quotes = lib_deps_in_quotes.replace("\\", "/")
+    nimcache_dir_in_quotes = f'"{path.join(prj_src_dir, "nimcache")}"'
+    fwd_slash_nimcache_dir_in_quotes = nimcache_dir_in_quotes.replace("\\", "/")
     options_to_append = (
-        ("path", fwd_slash_lib_deps),
+        ("path", fwd_slash_lib_deps_in_quotes),
         ("cpu", _get_cpu(env.subst("$PIOPLATFORM"))),
+        ("nimcache", fwd_slash_nimcache_dir_in_quotes),
     )
-    prj_dir = Path(env.subst("$PROJECT_DIR"))
     fwd_slash_config_path = str(prj_dir / "nim.cfg").replace("\\", "/")
     file_lines = open(fwd_slash_config_path).readlines()
     config_file_contents = ''.join(file_lines)
-    with open(fwd_slash_config_path, "a") as cfg_file:
-        for optn, val in options_to_append:
-            if optn not in config_file_contents:
-                cfg_file.write(f"{optn}:{val}\n")
+    if "# END nim-platformio" not in config_file_contents:
+        with open(fwd_slash_config_path, "a") as cfg_file:
+            cfg_file.write("# BEGIN nim-platformio\n")
+            for optn, val in options_to_append:
+                if optn not in config_file_contents:
+                    cfg_file.write(f"{optn}:{val}\n")
+            cfg_file.write("# END nim-platformio\n")
 
 
 def _get_cpu(pio_plat: str) -> str:
